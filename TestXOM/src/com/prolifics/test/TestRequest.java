b9e6d481753ed446f4cbda8766b51abcebd1cd4f
rahulwalia09 <rahulwalia1995@gmail.com>
2019-08-21 11:19:56 +0530

    commited for new repository

---
+++ TestXOM/src/com/prolifics/test/TestRequest.java
@@ -0,0 +1,14 @@
+package com.prolifics.test;
+
+public class TestRequest {
+
+	private String input="";
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
---
+++ TestXOM/src/com/prolifics/test/TestResponse.java
@@ -0,0 +1,14 @@
+package com.prolifics.test;
+
+public class TestResponse {
+
+	private String output="";
+
+	public String getOutput() {
+		return output;
+	}
+
+	public void setOutput(String output) {
+		this.output = output;
+	}
+}
